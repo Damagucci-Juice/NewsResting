1a717f0f9aeb0cb0933c2047e947e8eb83d7c3eb
Damagucci-Juice <jsr9874@gmail.com>
2023-01-18 22:26:57 +0900

    feat: NewsListViewController에 title만 보이도록 설정

--- NewsResting/Presentation/NewsList/ViewModel/NewsListViewModel.swift
+++ NewsResting/Presentation/NewsList/ViewModel/NewsListViewModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+protocol NewsListViewModelDelegate {
+    func completedLoad()
+}
+
+final class NewsListViewModel {
+    var newsViewModel: [NewsViewModel] = []
+    
+    let newsRepository: NewsRepository
+    
+    var delegate: NewsListViewModelDelegate?
+    
+    init(newsRepository: NewsRepository) {
+        self.newsRepository = newsRepository
+    }
+    
+}
+
+extension NewsListViewModel {
+    subscript(_ index: Int) -> NewsViewModel {
+        newsViewModel[index]
+    }
+    
+    func fetchNews(_ search: String) {
+        let newsQuery = NewsQuery(query: search)
+        newsRepository.fetchNewsList(query: newsQuery) { [unowned self] result in
+            switch result {
+            case .success(let newsList):
+                self.newsViewModel = newsList.articles.map {
+                    NewsViewModel(news: $0)
+                }
+                DispatchQueue.main.async {
+                    self.delegate?.completedLoad()
+                }
+            case .failure(let error):
+                debugPrint(error)
+            }
+        }
+    }
+    
+    var cellHeight: CGFloat {
+        return 85
+    }
+}
