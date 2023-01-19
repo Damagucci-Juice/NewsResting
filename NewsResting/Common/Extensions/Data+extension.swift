cfdc08c1cab23a4c164c81f48407f8dace1e740c
Damagucci-Juice <jsr9874@gmail.com>
2023-01-19 18:03:42 +0900

    refactor: Extension 파일 따로 분리

---
+++ NewsResting/Common/Extensions/Data+extension.swift
@@ -0,0 +1,18 @@
+//
+//  Data+extension.swift
+//  NewsResting
+//
+//  Created by YEONGJIN JANG on 2023/01/19.
+//
+
+import Foundation
+
+extension Data {
+    var prettyPrintedJSONString: NSString? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
+    }
+}
