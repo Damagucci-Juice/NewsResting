dfba4f86ce351e526e026ef722465b47679f45d7
Damagucci-Juice <jsr9874@gmail.com>
2023-01-19 18:25:56 +0900

    add: NetworkRequest 타입

---
+++ NewsResting/Infrastructure/Network/NetworkReqeust.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkReqeust.swift
+//  NewsResting
+//
+//  Created by YEONGJIN JANG on 2023/01/19.
+//
+
+import Foundation
+
+protocol NetworkRequest: AnyObject {
+    associatedtype ModelType
+    
+    func decode(_ data: Data) -> ModelType?
+    func excute(withCompletion completion: @escaping (ModelType?) -> Void)
+}
+
+extension NetworkRequest {
+    fileprivate func load(_ urlRequest: URLRequest, withCompletion completion: @escaping (ModelType?) -> Void) {
+        let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, _, _ in
+            guard let data = data, let value = self?.decode(data) else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                completion(value)
+            }
+        }
+        task.resume()
+    }
+}
