2da537e7345b3effc1f905166262d86277456985
Damagucci-Juice <jsr9874@gmail.com>
2023-01-31 16:36:42 +0900

    feat: Query에 필요한 Repository, CoreDataStorage 추가
    #4

---
+++ NewsResting/Data/PersistentStorage/CoreDataStorage/CoreDataStorage.swift
@@ -0,0 +1,58 @@
+//
+//  CoreDataStorage.swift
+//  NewsResting
+//
+//  Created by YEONGJIN JANG on 2023/01/31.
+//
+
+import CoreData
+
+enum CoreDataStorageError: Error {
+    case readError(Error)
+    case saveError(Error)
+    case deleteError(Error)
+}
+
+class CoreDataStorage {
+
+    public static let modelName = "CoreDataStorage"
+
+    public static let model: NSManagedObjectModel = {
+      // swiftlint:disable force_unwrapping
+      let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd")!
+      return NSManagedObjectModel(contentsOf: modelURL)!
+    }()
+    
+    static let shared = CoreDataStorage()
+    
+    init() {}
+
+    // MARK: - Core Data stack
+    public lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: CoreDataStorage.modelName)
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                // TODO: - Log to Crashlytics
+                assertionFailure("CoreDataStorage Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // TODO: - Log to Crashlytics
+                assertionFailure("CoreDataStorage Unresolved error \(error), \((error as NSError).userInfo)")
+            }
+        }
+    }
+
+    func performBackgroundTask(_ block: @escaping (NSManagedObjectContext) -> Void) {
+        persistentContainer.performBackgroundTask(block)
+    }
+}
