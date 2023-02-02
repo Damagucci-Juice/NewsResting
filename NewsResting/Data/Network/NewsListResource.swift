b93de12ffe5e00a261511a3405a985833bdc5fac
Damagucci-Juice <jsr9874@gmail.com>
2023-02-02 17:01:11 +0900

    feat: Resource 타입에서 케이스 추가에 따른 변경사항 및 APIResource 수정
    - path 프로퍼티에 "&" 여부에 따라 붙여줄 수 있도록 설정
    - NewsListResource에 category case 추가
    #5

--- NewsResting/Data/Network/NewsListResource.swift
+++ NewsResting/Data/Network/NewsListResource.swift
@@ -9,24 +9,42 @@ import Foundation
 
 enum NewsListResource {
     case search(key: String)
+    case category(NewsCategory) // "https://newsapi.org/v2/top-headlines?country=kr&category=sports"
+//GET https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=0c4d4b75321642f78aa273d2c23d021c
+
 }
 
 extension NewsListResource: APIResource {
     
     typealias ModelType = NewsList
 
+    // TODO: - UserDefault로 저장해서 할지, 아니면 나라별 탑 헤드라인을 서비스할 지 고민
+    static let defaultCountry = "kr"
+    
     var baseURL: String {
         "https://newsapi.org/v2/"
     }
 
+    //TODO: - "?"를 path 뒤에 항상 처렇게 붙여줘야하는것인지, 아니면 URL Request만들 때 추가해줄 수 있는지 검토
     var path: String {
-        "everything?"
+        switch self {
+        case .search(_):
+            return "everything?"
+        case .category(_):
+            return "top-headlines?"
+        }
     }
 
+    //TODO: - 키와 벨류 사이엔 "=", 키와 키 사이엔 "&"
     var query: [String : String] {
         switch self {
         case .search(let key):
             return ["q": key]
+        case .category(let category):
+            return [
+                "country": NewsListResource.defaultCountry,
+                "category": category.rawValue
+            ]
         }
     }
 
