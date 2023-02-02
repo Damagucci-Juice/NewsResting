acc362e8238a17f627c88872cb9bb5fe79e2f652
Damagucci-Juice <jsr9874@gmail.com>
2023-02-02 17:07:46 +0900

    refactor: 코드 배치 조정
    - 네트워크 에러여서 Infrastructure - Network / 이동
    #5

---
+++ NewsResting/Infrastructure/Network/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  NewsResting
+//
+//  Created by YEONGJIN JANG on 2023/02/02.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case urlGeneration
+    case decodingFailure
+}
