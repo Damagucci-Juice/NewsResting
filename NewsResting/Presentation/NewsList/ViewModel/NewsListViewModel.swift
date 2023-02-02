c3d329b1b8d9726e7ae23a7a5bbf5c254af0b182
Damagucci-Juice <jsr9874@gmail.com>
2023-02-02 17:07:00 +0900

    feat: 카테고리 페치 추가 및 캡쳐 방지 위해 unowned self 로 변경
    #5

--- NewsResting/Presentation/NewsList/ViewModel/NewsListViewModel.swift
+++ NewsResting/Presentation/NewsList/ViewModel/NewsListViewModel.swift
@@ -27,24 +27,25 @@ extension NewsListViewModel {
         self.onUpdated = completion
     }
     
+    //TODO: - 여기서 뷰 모델로 맵핑하는것이 맞는가? 아니면 UseCase에서 child ViewModel을 매핑하는게 맞는가?
     public func fetchNews(_ search: String) {
         let newsQuery = NewsQuery(query: search)
-        newsRepository.fetchNews(with: newsQuery) { [weak self] result in
+        newsRepository.fetchNews(with: newsQuery) { [unowned self] result in
             guard let newsList = result?.articles else { return }
-            self?.newsViewModel = newsList.map { NewsViewModel(news: $0)}
-            self?.onUpdated()
+            newsViewModel = newsList.map { $0.toViewModel() }
+            onUpdated()
         }
     }
     
-    public func fetchNews(with category: String) {
-        
+    public func fetchNews(with category: NewsCategory) {
+        newsRepository.fetchNews(with: category) { [unowned self] result in
+            guard let newsList = result?.articles else { return }
+            newsViewModel = newsList.map { $0.toViewModel() }
+            onUpdated()
+        }
     }
     
     var cellHeight: CGFloat {
         return 85
     }
-}
-
-enum NetworkError: Error {
-    case urlGeneration
 }
