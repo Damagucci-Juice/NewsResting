0afb111aae2bdca178a88e7a71c53064d3c8a68f
Damagucci-Juice <jsr9874@gmail.com>
2023-02-02 17:05:16 +0900

    feat: 카테고리 fetch 함수 도입 및 implement 객체에서 코드 정리
    #5

--- NewsResting/Data/Repositories/NewsRepositoryImpl.swift
+++ NewsResting/Data/Repositories/NewsRepositoryImpl.swift
@@ -9,17 +9,11 @@ import Foundation
 
 final class NewsRepositoryImpl {
     private var apiReqeust: APIRequest<NewsListResource>?
-    
-    private func makeNewsListResource(_ query: NewsQuery) -> NewsListResource {
-        return NewsListResource.search(key: query.query)
-    }
-    
-    private func makeNewsAPIRequest(_ resource: NewsListResource) -> APIRequest<NewsListResource> {
-        return APIRequest(resource: resource)
-    }
 }
 
+//MARK: - Public
 extension NewsRepositoryImpl: NewsRepository {
+    //TODO: - 두 펑션이 파라미터만 차이가 있지 사실상 하는일은 동일하여 중복되고 있음, 개선 방안이 있을까?
     //MARK: 쿼리를 이용해 뉴스 페치
     func fetchNews(with query: NewsQuery, completion: @escaping (NewsList?) -> Void) {
         let newsListResource = makeNewsListResource(query)
@@ -27,4 +21,25 @@ extension NewsRepositoryImpl: NewsRepository {
         self.apiReqeust?.excute(withCompletion: completion)
     }
     //MARK: 카테고리를 이용해 뉴스 페치(분야별 뉴스)
+    func fetchNews(with category: NewsCategory, completion: @escaping (NewsList?) -> Void) {
+        let newsListResource = makeNewsListResourece(category)
+        self.apiReqeust = makeNewsAPIRequest(newsListResource)
+        self.apiReqeust?.excute(withCompletion: completion)
+    }
+}
+
+//MARK: - Private
+extension NewsRepositoryImpl {
+    //TODO: 나라별, 언론사별 Top HeadLine이 추가될 것인데, 그 때마다 make 함수가 추가될 것인가?
+    private func makeNewsListResource(_ query: NewsQuery) -> NewsListResource {
+        return NewsListResource.search(key: query.query)
+    }
+    
+    private func makeNewsListResourece(_ category: NewsCategory) -> NewsListResource {
+        return NewsListResource.category(category)
+    }
+    
+    private func makeNewsAPIRequest(_ resource: NewsListResource) -> APIRequest<NewsListResource> {
+        return APIRequest(resource: resource)
+    }
 }
--- NewsResting/Domain/Interfaces/Repositories/NewsRepository.swift
+++ NewsResting/Domain/Interfaces/Repositories/NewsRepository.swift
@@ -9,6 +9,5 @@ import Foundation
 
 protocol NewsRepository {
     func fetchNews(with query: NewsQuery, completion: @escaping (NewsList?) -> Void)
-    //TODO: 카테고리별 뉴스 도입
-//    func fetchNews(with category: NewsCategory, completion: @escaping (NewsList?) -> Void)
+    func fetchNews(with category: NewsCategory, completion: @escaping (NewsList?) -> Void)
 }
