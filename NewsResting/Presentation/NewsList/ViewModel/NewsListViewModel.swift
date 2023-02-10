a574eff63f1df1a52118c1a92347432541da0aac
Damagucci-Juice <jsr9874@gmail.com>
2023-02-10 17:25:21 +0900

    chore: ViewModel이 크게 바뀌기 전에 주석처리
    #8

--- NewsResting/Presentation/NewsList/ViewModel/NewsListViewModel.swift
+++ NewsResting/Presentation/NewsList/ViewModel/NewsListViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 final class NewsListViewModel {
     var newsViewModel: [NewsViewModel] = []
     
+    //TODO: - 여기 UseCase 도입하면서 완전히 바뀜 1
     private var newsRepository: NewsRepository
     private var onUpdated: () -> Void = { }
     
@@ -27,23 +28,23 @@ extension NewsListViewModel {
         self.onUpdated = completion
     }
     
-    //TODO: - 여기서 뷰 모델로 맵핑하는것이 맞는가? 아니면 UseCase에서 child ViewModel을 매핑하는게 맞는가?
-    public func fetchNews(_ search: String) {
-        let newsQuery = NewsQuery(query: search)
-        newsRepository.fetchNews(with: newsQuery) { [unowned self] result in
-            guard let newsList = result?.articles else { return }
-            newsViewModel = newsList.map { $0.toViewModel() }
-            onUpdated()
-        }
-    }
-    
-    public func fetchNews(with category: NewsCategory) {
-        newsRepository.fetchNews(by: category) { [unowned self] result in
-            guard let newsList = result?.articles else { return }
-            newsViewModel = newsList.map { $0.toViewModel() }
-            onUpdated()
-        }
-    }
+    //TODO: - 여기 UseCase 도입하면서 완전히 바뀜 2
+//    public func fetchNews(_ search: String) {
+//        let newsQuery = NewsQuery(query: search)
+//        newsRepository.fetchNews(with: newsQuery) { [unowned self] result in
+//            guard let newsList = result?.articles else { return }
+//            newsViewModel = newsList.map { $0.toViewModel() }
+//            onUpdated()
+//        }
+//    }
+//
+//    public func fetchNews(with category: NewsCategory) {
+//        newsRepository.fetchNews(by: category) { [unowned self] result in
+//            guard let newsList = result?.articles else { return }
+//            newsViewModel = newsList.map { $0.toViewModel() }
+//            onUpdated()
+//        }
+//    }
     
     var cellHeight: CGFloat {
         return 85
