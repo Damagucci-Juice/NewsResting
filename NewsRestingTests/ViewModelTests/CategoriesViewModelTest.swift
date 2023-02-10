246a648274e99e36364c24ba497e3f2ec5e16842
Damagucci-Juice <jsr9874@gmail.com>
2023-02-10 23:23:09 +0900

    add: ViewModel 파일 추가 및 Test 파일 추가
    #25

---
+++ NewsRestingTests/ViewModelTests/CategoriesViewModelTest.swift
@@ -0,0 +1,8 @@
+//
+//  CategoriesViewModelTest.swift
+//  NewsRestingTests
+//
+//  Created by YEONGJIN JANG on 2023/02/10.
+//
+
+import Foundation
---
+++ NewsRestingTests/ViewModelTests/RecentQueriesViewModelTest.swift
@@ -0,0 +1,8 @@
+//
+//  RecentQueriesViewModel.swift
+//  NewsRestingTests
+//
+//  Created by YEONGJIN JANG on 2023/02/10.
+//
+
+import Foundation
