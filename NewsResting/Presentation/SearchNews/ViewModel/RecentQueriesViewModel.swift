827516e331a02dd057f9921287ac2965832052c2
Damagucci-Juice <jsr9874@gmail.com>
2023-02-10 23:30:54 +0900

    feat: 최근 쿼리를 가져오는 뷰모델 추가
    #25

--- NewsResting/Presentation/SearchNews/ViewModel/RecentQueriesViewModel.swift
+++ NewsResting/Presentation/SearchNews/ViewModel/RecentQueriesViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+final class RecentQueriesViewModel {
+    private let fetchRecentQueriesUseCase: FetchRecentQueriesUseCase
+    private(set) var queries: [NewsQuery] = []
+    
+    init(fetchRecentQueriesUseCase: FetchRecentQueriesUseCase) {
+        self.fetchRecentQueriesUseCase = fetchRecentQueriesUseCase
+        Task {
+            try await fetchQuereis()
+        }
+    }
+}
+
+extension RecentQueriesViewModel {
+    func fetchQuereis() async throws {
+        do {
+            queries = try await fetchRecentQueriesUseCase.excute()
+        } catch {
+            throw NetworkError.fetchQuriesFailure
+        }
+    }
+}
