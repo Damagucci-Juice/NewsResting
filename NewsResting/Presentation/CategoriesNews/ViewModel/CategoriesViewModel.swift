d18ffb9b57a551681bb0b400eff1a84fba0aaea6
Damagucci-Juice <jsr9874@gmail.com>
2023-02-10 18:36:28 +0900

    feat: CategoriesViewModel 구현
    - initCategoriesVM 이런식으로 네이밍을 해도 괜찮을지 모르겠음
    #25

--- NewsResting/Presentation/CategoriesNews/ViewModel/CategoriesViewModel.swift
+++ NewsResting/Presentation/CategoriesNews/ViewModel/CategoriesViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+final class CategoriesViewModel {
+    let useCase: FetchCategoriesUseCase
+    private var categories: [NewsCategory: NewsList] = [:]
+    
+    init(useCase: FetchCategoriesUseCase) {
+        self.useCase = useCase
+        Task {
+            try await initCategoriesVM()
+        }
+    }
+}
+
+extension CategoriesViewModel {
+    subscript(_ category: NewsCategory) -> NewsList {
+        return categories[category] ?? NewsList(totalResults: 0, articles: [])
+    }
+    
+    func initCategoriesVM() async throws {
+        do {
+            self.categories = try await useCase.excute()
+        } catch {
+            throw NetworkError.fetchCategoriesFailure
+        }
+    }
+}
