792fd259e019c05761b4f0d17e35ff2c820c153d
Damagucci-Juice <jsr9874@gmail.com>
2023-02-10 23:25:36 +0900

    refactor: 불필요한 메서드 삭제
    #25

--- NewsResting/Domain/Interfaces/Repositories/NewsQueryRepository.swift
+++ NewsResting/Domain/Interfaces/Repositories/NewsQueryRepository.swift
@@ -8,11 +8,7 @@
 import Foundation
 
 protocol NewsQueryRepository {
-    //TODO: Result 타입으로 변경, Error 케이스 추가
-    //TODO: withContinuation으로 랩핑된 것이랑 completion 버전 메서드랑 혼용해도 되는가에 대한 문제
-    func fetchRecentQuery(maxCount: Int, completion: @escaping ([NewsQuery]?) -> Void)
     func fetchRecentQuery(maxCount: Int) async throws -> [NewsQuery]
-    func saveQuery(query: NewsQuery, completion: @escaping (NewsQuery?) -> Void)
     @discardableResult
     func saveQuery(query: NewsQuery) async throws -> NewsQuery
 }
