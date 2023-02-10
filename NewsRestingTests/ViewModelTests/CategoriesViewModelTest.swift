328464e4da39addcd7f49b7a3eec72a393034565
Damagucci-Juice <jsr9874@gmail.com>
2023-02-10 23:35:33 +0900

    test: 여러 카테고리가 Fetch 되는지 테스트
    #25

--- NewsRestingTests/ViewModelTests/CategoriesViewModelTest.swift
+++ NewsRestingTests/ViewModelTests/CategoriesViewModelTest.swift
@@ -5,4 +5,44 @@
 //  Created by YEONGJIN JANG on 2023/02/10.
 //
 
-import Foundation
+import XCTest
+@testable import NewsResting
+
+final class CategoriesViewModelTest: XCTestCase {
+    
+    var coreDataStorage: CoreDataStorage!
+    var responseCache: NewsResponseStorage!
+    var repository: NewsRepository!
+    var usecase: FetchCategoriesUseCase!
+    var categoryViewModel: CategoriesViewModel!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        coreDataStorage = TestCoreDataStorage()
+        responseCache = CoreDataNewsResponseStorage(coreDatastorage: coreDataStorage)
+        repository = NewsRepositoryImpl(responseCache: responseCache)
+        usecase = FetchCategoriesUseCaseImpl(newsRepository: repository)
+        categoryViewModel = CategoriesViewModel(useCase: usecase)
+    }
+    
+    override func tearDownWithError() throws {
+        coreDataStorage = nil
+        responseCache = nil
+        repository = nil
+        usecase = nil
+        categoryViewModel = nil
+        try super.tearDownWithError()
+    }
+    
+    func testStart() async throws {
+        try await categoryViewModel.start()
+        
+        try await Task.sleep(nanoseconds: NSEC_PER_SEC * 2)
+        
+        for category in NewsCategory.allCases {
+            let newsList = categoryViewModel[category]
+            XCTAssertFalse(newsList.articles.isEmpty)
+            XCTAssertNotEqual(newsList.totalResults, 0)
+        }
+    }
+}
