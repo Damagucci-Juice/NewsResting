9190c9ac07b1fc86ee5dc348c27ceaa3ceff84c5
Damagucci-Juice <jsr9874@gmail.com>
2023-02-11 18:27:03 +0900

    feat: Localize 편안하게 하기위해 Extension 추가
    #15

---
+++ NewsResting/Presentation/Common/Extensions/String+extension.swift
@@ -0,0 +1,14 @@
+//
+//  String+extension.swift
+//  NewsResting
+//
+//  Created by YEONGJIN JANG on 2023/02/11.
+//
+
+import Foundation
+
+extension String {
+    func localized(comment: String = "") -> String {
+        return NSLocalizedString(self, comment: comment)
+    }
+}
