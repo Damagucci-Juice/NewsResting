6cbcf09e54e0c07289db262c7a5014eeea8c005d
Damagucci-Juice <jsr9874@gmail.com>
2023-02-15 16:42:22 +0900

    add: SearchViewController 생성
    #38

---
+++ NewsResting/Presentation/SearchNews/View/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  NewsResting
+//
+//  Created by YEONGJIN JANG on 2023/02/15.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
