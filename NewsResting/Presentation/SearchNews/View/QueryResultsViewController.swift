994494e34d6d2b0eb48ad3d2c1c8f9417eff4f91
Damagucci-Juice <jsr9874@gmail.com>
2023-02-15 23:00:23 +0900

    add: QueryResultsViewController와 QueryTableViewCell 추가
    #38

---
+++ NewsResting/Presentation/SearchNews/View/QueryResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QueryResultsViewController.swift
+//  NewsResting
+//
+//  Created by YEONGJIN JANG on 2023/02/15.
+//
+
+import UIKit
+
+class QueryResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NewsResting/Presentation/SearchNews/View/QueryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  QueryTableViewCell.swift
+//  NewsResting
+//
+//  Created by YEONGJIN JANG on 2023/02/15.
+//
+
+import UIKit
+
+class QueryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
