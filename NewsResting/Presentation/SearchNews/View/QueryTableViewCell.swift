f53c84c68d5d29df29454276655ac0f455a0880d
Damagucci-Juice <jsr9874@gmail.com>
2023-02-15 23:02:32 +0900

    feat: SearchVC 에서 기본 쿼리 보여주도록 설정
    #38

--- NewsResting/Presentation/SearchNews/View/QueryTableViewCell.swift
+++ NewsResting/Presentation/SearchNews/View/QueryTableViewCell.swift
@@ -8,16 +8,31 @@
 import UIKit
 
 class QueryTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private let queryLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func setupLayout() {
+        addSubview(queryLabel)
+        
+        queryLabel.snp.makeConstraints {
+            $0.top.bottom.leading.equalToSuperview()
+        }
+    }
+    
+    public func fillUp(_ query: String) {
+        self.queryLabel.text = query
+    }
 }
+
--- NewsResting/Presentation/SearchNews/View/SearchViewController.swift
+++ NewsResting/Presentation/SearchNews/View/SearchViewController.swift
@@ -6,11 +6,95 @@
 //
 
 import UIKit
+import SnapKit
+
+
 
 class SearchViewController: UIViewController {
-
+    private var searchNewsViewModel = SearchNewsViewModel(usecase: SearchNewsUseCaseImpl(newsRepository: NewsRepositoryImpl(responseCache: CoreDataNewsResponseStorage())))
+    
+    private var recentQueriesViewModel = RecentQueriesViewModel(fetchRecentQueriesUseCase: FetchRecentQueriesUseCaseImpl(queryRepository: NewsQueryRepositoryImpl(newsQueryStorage: CoreDataNewsQueryStorage())))
+    
+    var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(QueryTableViewCell.self, forCellReuseIdentifier: QueryTableViewCell.reusableIdentifier)
+        return tableView
+    }()
+    
+    var searchController: UISearchController?
+    
+    //MARK: - View Contoller's life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        setupLayout()
+        setupAttribute()
+        setupNavigation()
+    }
+}
+
+//MARK: - Private
+extension SearchViewController {
+    private func setupLayout() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setupAttribute() {
+        searchController = UISearchController(searchResultsController: QueryResultsViewController(recentQueriesViewModel: recentQueriesViewModel))
+        view.backgroundColor = .white
+        searchController?.searchResultsUpdater = self
+        searchController?.searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func setupNavigation() {
+        navigationItem.searchController = searchController
+        navigationItem.largeTitleDisplayMode = .always
+        navigationItem.title = "search term".localized()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        recentQueriesViewModel.queries.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: QueryTableViewCell.reusableIdentifier, for: indexPath) as? QueryTableViewCell else { return UITableViewCell() }
+        let viewModel = recentQueriesViewModel.queries[indexPath.row]
+        cell.fillUp(viewModel.query)
+        return cell
+    }
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        <#code#>
+//    }
+
+}
+
+//MARK: - Public
+extension SearchViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text,
+              let resultVC = searchController.searchResultsController as? QueryResultsViewController
+        else { return }
+        resultVC.loadInitialQueries(text)
+        print(text)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text else { return }
+        Task {
+            let viewModel = try await searchNewsViewModel.getNews(searchTerm)
+            self.navigationController?.pushViewController(NewsListViewController(newsListViewModel: viewModel), animated: true)
+        }
     }
 }
