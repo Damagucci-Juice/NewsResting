f849f8ac526228346cc3d48709d3effa3c53b4f3
Damagucci-Juice <jsr9874@gmail.com>
2023-02-20 16:51:34 +0900

    feat: DetailViewController 이미지랑 타이틀만 구현
    - ViewModel을 받을 수 있도록 의존성 설정
    - imageRepository 에 대한 의존성을 어떻게 하면 좋을지 고민
    #53

--- NewsResting/Presentation/Detail/View/DetailNewsViewController.swift
+++ NewsResting/Presentation/Detail/View/DetailNewsViewController.swift
@@ -6,24 +6,81 @@
 //
 
 import UIKit
+import SnapKit
 
 class DetailNewsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private let viewModel: NewsItemViewModel
+    private let imageRepository = ImageRepositoryImpl()
+    private var isFetchImageSussecc = true
+    
+    private lazy var imageView = UIImageView()
+    private var imageViewHeight: Constraint?
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    init(newsItemViewModel: NewsItemViewModel) {
+        self.viewModel = newsItemViewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLayout()
+        setupAttribute()
+        fillContents()
+    }
+}
 
+extension DetailNewsViewController {
+    private func setupLayout() {
+        view.addSubview(imageView)
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(15)
+            make.leading.trailing.equalToSuperview().inset(15)
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            self.imageViewHeight = make.height.equalTo(300).constraint
+        }
+    }
+    
+    private func setupAttribute() {
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .gray
+        view.backgroundColor = .white
+    }
+    
+    private func fillContents() {
+        Task {
+            if let imagePath = viewModel.imagePath {
+                let start = Date.timeIntervalSinceReferenceDate
+                let data = try await imageRepository.fetchImage(path: imagePath)
+                imageView.image = UIImage(data: data)
+            } else {
+                //MARK: ImageView 조정
+                closeImageView()
+            }
+        }
+        titleLabel.text = viewModel.title
+    }
+    
+    private func closeImageView() {
+        self.imageViewHeight?.update(offset: 0)
+        view.layoutIfNeeded()
+    }
 }
